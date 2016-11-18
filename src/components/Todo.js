66663ea2b6ee500bc1052527f5f65997aab60fe0
Raj <rajeev.v.singh@gmail.com>
2016-11-17 23:37:14 -0500

    Add toggleTodo function to Todo, Expand on template

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Todo extends Component{
-
+  toggleTodo(e) {
+    this.props.actions.toggleTodo(this.props.id);
+  }
 
   render(){
     return(
-      <li>
-        {this.id}
+      <li className={ this.props.completed ? 'completed' : 'not-completed' }>
+        {this.props.text}
+        {this.props.completed ? ' (completed)' : ' (not completed)' }
+        <input type="checkbox" onClick={this.toggleTodo.bind(this)} />
       </li>
     )
-
-}
-
+  }
 }
