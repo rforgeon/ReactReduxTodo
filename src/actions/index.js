f58b495e6396545f3b6dd9a130c09c4844e2eb14
Raj <rajeev.v.singh@gmail.com>
2016-11-17 23:35:06 -0500

    Make action arguments consistent with action properties in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes';
 
-export const addTodo = todo => ({type: types.ADD_TODO, todo});
-export const toggleTodo = toggle=> ({type: types.TOGGLE_TODO, toggle});
+export const addTodo = text => ({type: types.ADD_TODO, text});
+export const toggleTodo = id=> ({type: types.TOGGLE_TODO, id});
 export const showAll = showAll => ({type: types.SHOW_ALL, showAll});
 export const showActive = showActive => ({type: types.SHOW_ACTIVE, showActive});
 export const showCompleted = showCompleted => ({type: types.SHOW_COMPLETED, showCompleted});
